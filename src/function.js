57257e125bff87cec3c473cb115b3d05e3b5f8cc
Bryan <mhdalana211201@gmail.com>
2022-05-27 18:37:51 +0700

    menambahkan fitur darkmode, mengubah tampilan, warna, dan item

--- src/function.js
+++ src/function.js
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import "./styles/themes.css";
 import "./styles/main-style.css";
 import main from "./scripts/app-main.js";
+
+const btnTheme = document.querySelector(".btn-theme");
+
+btnTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+});
 
 main();
