f19ff01b34f8373f644c48a5d8260f1839285d9c
Bryan <mhdalana211201@gmail.com>
2022-06-01 18:40:22 +0700

    menghapus data API

--- src/scripts/Data/data-files.js
+++ src/scripts/Data/data-files.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
-class geoDataset {
-  static url =
-    "https://katalog.data.go.id/api/action/package_show?id=f5e0048f-c888-4f97-998b-3bba6292f7cb";
+// class geoDataset {
+//   static url =
+//     "https://katalog.data.go.id/api/action/package_show?id=f5e0048f-c888-4f97-998b-3bba6292f7cb";
 
-  static dataSet = () => {
-    return axios
-      .get(this.url)
-      .then((response) => response.data.result.organization)
-      .catch((error) => error);
-  };
-}
+//   static dataSet = () => {
+//     return axios
+//       .get(this.url)
+//       .then((response) => response.data.result.organization)
+//       .catch((error) => error);
+//   };
+// }
 
-export default geoDataset;
+// export default geoDataset;
