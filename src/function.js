bc354667ff38e91c8e8308354845136156a495e7
Bryan <mhdalana211201@gmail.com>
2022-06-02 19:34:33 +0700

    menambahkan carousel, menambahkan searchbox, menambahkan logo, dan styling

--- src/function.js
+++ src/function.js
@@ -3,7 +3,13 @@ import "bootstrap";
 import "./styles/themes.css";
 import "./styles/main-style.css";
 import main from "./scripts/app-main.js";
+import logo from "./media/logo.png";
 
+// Import Logo Image
+const banner = document.querySelector(".logo");
+banner.setAttribute("src", `${logo}`);
+
+// Theme Button Toggle
 const btnTheme = document.querySelector(".btn-theme");
 
 btnTheme.addEventListener("click", () => {
