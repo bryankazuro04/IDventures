6437f243fbfcbc228c5ffdafbb4203e98148c1e2
Bryan <mhdalana211201@gmail.com>
2022-06-05 14:21:25 +0700

    menambahkan responsif pada navbar

--- src/function.js
+++ src/function.js
@@ -2,6 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "./styles/themes.css";
 import "./styles/main-style.css";
+import "./styles/res-1280px.css";
+import "./styles/res-768px.css";
+import "./styles/res-576px.css";
 import main from "./scripts/app-main.js";
 import logo from "./media/logo.png";
 
