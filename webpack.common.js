bffdc2c7f3621a48126fa8eb452226df4c309bca
Bryan <mhdalana211201@gmail.com>
2022-06-10 07:55:27 +0700

    Menambahkan plugin Copy-Webpack & config

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/function.js",
@@ -18,12 +19,28 @@ module.exports = {
           loader: "file-loader",
         }, ],
       },
+      {
+        test: /\.(html)$/,
+        include: path.join(__dirname, "src/partials"),
+        use: {
+          loader: "html-loader",
+          options: {
+            interpolate: true,
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      filename: path.resolve(__dirname, "./dist/index.html"),
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, "./src/media"),
+        to: path.resolve(__dirname, "./dist/media"),
+      }, ],
     }),
   ],
 };
