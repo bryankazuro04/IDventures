fd4be78f22ed7f0763faf73bcf326ef8132ce21a
Bryan <mhdalana211201@gmail.com>
2022-06-12 22:25:15 +0700

    renaming file tempat wisata & changed config in webpack common

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
         use: {
           loader: "html-loader",
           options: {
-            interpolate: true,
+            interpolate: false,
           },
         },
       },
