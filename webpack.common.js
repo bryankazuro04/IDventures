3780ef719566f8f9e2a4b9b945246bbdb496f3fd
Bryan <mhdalana211201@gmail.com>
2022-06-18 20:11:13 +0700

    memperbaiki config html plugin pada webpack common

--- webpack.common.js
+++ webpack.common.js
@@ -28,12 +28,12 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [{
-          from: path.resolve(__dirname, "./src/media"),
-          to: path.resolve(__dirname, "./dist/media"),
+          from: path.resolve(__dirname, "./src/media/"),
+          to: path.resolve(__dirname, "./dist/media/"),
         },
         {
-          from: path.resolve(__dirname, "./src/page"),
-          to: path.resolve(__dirname, "./dist/page"),
+          from: path.resolve(__dirname, "./src/page/"),
+          to: path.resolve(__dirname, "./dist/page/"),
         },
         {
           from: "./src/styles/wisata.css",
@@ -42,6 +42,14 @@ module.exports = {
         {
           from: "./src/styles/about-us.css",
           to: "./about-us.css",
+        },
+        {
+          from: "./src/styles/themes.css",
+          to: "./about-us.css",
+        },
+        {
+          from: "./src/scripts/another-main.js",
+          to: "./another-main.css",
         },
       ],
     }),
