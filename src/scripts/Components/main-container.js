599e62ac8bcfbf4eb5ae458fca38f0dc415ec793
Bryan <mhdalana211201@gmail.com>
2022-06-23 20:34:25 +0700

    menghapus import direct

--- src/scripts/Components/main-container.js
+++ src/scripts/Components/main-container.js
@@ -1,28 +1,9 @@
-import "./partials/section_1";
-import "./partials/section_2";
-import "./partials/section_3";
-import "./partials/section_4";
-
 class MainContainer extends HTMLElement {
   connectedCallback() {
     this.render();
   }
 
-  render() {
-    this.innerHTML = `
-    <best-recommendation class="section-1"></best-recommendation>
-    <hr />
-
-    <destination-recommendation class="portfolio pb-4" id="portfolio"></destination-recommendation>
-    <hr />
-
-    <gallery-section id="galeri" class="carousel slide carousel-fade p-5" data-bs-ride="carousel"></gallery-section>
-    <hr />
-
-    <nearest-destination class="row col-lg-12 section-4"></nearest-destination>
-    <hr />
-    `;
-  }
+  render() {}
 }
 
 customElements.define("main-container", MainContainer);
