378a1d233eed036636924eda40aaee1feae7d2b4
ayunovita123 <ayunovita7303@gmail.com>
2022-06-25 16:19:52 +0700

    Divider Section

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -24,26 +24,31 @@ const Home = {
       <destination-recommendation class="portfolio pb-4" id="portfolio"></destination-recommendation>
       <hr />
 
-      <!-- Travel essentials section -->
-      <section class="py-5 bg-light">
+      <!-- Divider Section -->
+      <section class="py-5">
         <div class="container py-4">
-          <header class="text-center mb-5">
-            <h2>My travel essentials</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </header>
-          <div class="row text-center">
-            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/bag.e4c5f0da.png" alt="" height="150">
-                <h3 class="h5">Backpack</h3>
-                <p class="text-sm text-muted">Deserunt et ad culpa culpa dolore.</p></a></div>
-            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/camera.3ee186fd.png" alt="" height="150">
-                <h3 class="h5">Camera</h3>
-                <p class="text-sm text-muted">Consectetur ex sunt duis minim quis dolor.</p></a></div>
-            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/glasses.80147afd.png" alt="" height="150">
-                <h3 class="h5">Sunglasses</h3>
-                <p class="text-sm text-muted">Deserunt et ad culpa culpa dolore.</p></a></div>
-            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/headphone.cccb2497.png" alt="" height="150">
-                <h3 class="h5">Headphones</h3>
-                <p class="text-sm text-muted">Elit ad est labore irure qui.</p></a></div>
+          <!-- Home listing -->
+          <div class="row align-items-stretch gx-lg-0">
+            <div class="col-lg-6 order-2 order-lg-1 bg-full-left">
+              <div class="h-100 bg-light d-flex align-items-center">
+                <div class="py-5 px-4">
+                  <p class="text-primary font-weight-bold small text-uppercase mb-2">Travel guide</p>
+                  <h3 class="h4"> <a class="text-reset" href="post.html">Book to inspire your travel</a></h3>
+                  <div class="text-muted">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam nobis autem voluptate illum beatae atque suscipit inventore tenetur, perferendis facere sequi optio laudantium obcaecati aliquam, dolores ea. Pariatur, repellendus.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam nobis autem voluptate illum beatae atque suscipit inventore tenetur, perferendis facere sequi optio laudantium obcaecati aliquam, dolores ea. Pariatur, repellendus.</p>
+                  </div>
+                  <ul class="list-inline small text-uppercase mb-0">
+                    <li class="list-inline-item align-middle"><img class="rounded-circle shadow-sm" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/person-1.57fd03a8.jpg" alt="" width="30"/></li>
+                    <li class="list-inline-item me-0 text-gray align-middle">By </li>
+                    <li class="list-inline-item align-middle me-0"><a class="fw-bold reset-anchor" href="#!">Jason Doe</a></li>
+                    <li class="list-inline-item text-gray align-middle me-0">|</li>
+                    <li class="list-inline-item text-gray align-middle">Jan, 2019</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2"><a class="d-block h-100 bg-center bg-cover" href="post.html" style="background: url(https://d19m59y37dris4.cloudfront.net/travel/2-0/img/travel-home-divider.67659987.jpg)"></a></div>
           </div>
         </div>
       </section>
