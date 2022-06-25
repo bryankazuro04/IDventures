522f21d8ffe454232a05ba88d7c4ee6dba43fded
ayunovita123 <ayunovita7303@gmail.com>
2022-06-25 16:26:42 +0700

    menambah Instagram section

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -24,31 +24,46 @@ const Home = {
       <destination-recommendation class="portfolio pb-4" id="portfolio"></destination-recommendation>
       <hr />
 
-      <!-- Divider Section -->
-      <section class="py-5">
-        <div class="container py-4">
-          <!-- Home listing -->
-          <div class="row align-items-stretch gx-lg-0">
-            <div class="col-lg-6 order-2 order-lg-1 bg-full-left">
-              <div class="h-100 bg-light d-flex align-items-center">
-                <div class="py-5 px-4">
-                  <p class="text-primary font-weight-bold small text-uppercase mb-2">Travel guide</p>
-                  <h3 class="h4"> <a class="text-reset" href="post.html">Book to inspire your travel</a></h3>
-                  <div class="text-muted">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam nobis autem voluptate illum beatae atque suscipit inventore tenetur, perferendis facere sequi optio laudantium obcaecati aliquam, dolores ea. Pariatur, repellendus.</p>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam nobis autem voluptate illum beatae atque suscipit inventore tenetur, perferendis facere sequi optio laudantium obcaecati aliquam, dolores ea. Pariatur, repellendus.</p>
-                  </div>
-                  <ul class="list-inline small text-uppercase mb-0">
-                    <li class="list-inline-item align-middle"><img class="rounded-circle shadow-sm" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/person-1.57fd03a8.jpg" alt="" width="30"/></li>
-                    <li class="list-inline-item me-0 text-gray align-middle">By </li>
-                    <li class="list-inline-item align-middle me-0"><a class="fw-bold reset-anchor" href="#!">Jason Doe</a></li>
-                    <li class="list-inline-item text-gray align-middle me-0">|</li>
-                    <li class="list-inline-item text-gray align-middle">Jan, 2019</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 order-1 order-lg-2"><a class="d-block h-100 bg-center bg-cover" href="post.html" style="background: url(https://d19m59y37dris4.cloudfront.net/travel/2-0/img/travel-home-divider.67659987.jpg)"></a></div>
+      <!-- Instagram section-->
+      <section class="pb-5">
+        <div class="container pb-4">
+          <header class="text-center mb-5">
+            <h2>Backpack traveler</h2>
+            <p>A place for your Instagram pictures or gallery.</p>
+          </header>
+          <div class="row">
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-1.2aa0ba43.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-2.732bba03.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-3.0bbf93cd.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-4.95e66b76.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-5.3813454e.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-6.02c6ec6f.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-1.2aa0ba43.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
+            <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-2.732bba03.jpg" alt="">
+                <div class="instagram-item-overlay p-5">
+                  <h6>We travel not to escape life, but for life not to escape us.</h6>
+                </div></a></div>
           </div>
         </div>
       </section>
