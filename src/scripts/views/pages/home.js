21370f38c6a828259d9bdc3437acf652c1c08261
ayunovita123 <ayunovita7303@gmail.com>
2022-06-25 16:04:35 +0700

    Travel essentials section

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -24,6 +24,30 @@ const Home = {
       <destination-recommendation class="portfolio pb-4" id="portfolio"></destination-recommendation>
       <hr />
 
+      <!-- Travel essentials section -->
+      <section class="py-5 bg-light">
+        <div class="container py-4">
+          <header class="text-center mb-5">
+            <h2>My travel essentials</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </header>
+          <div class="row text-center">
+            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/bag.e4c5f0da.png" alt="" height="150">
+                <h3 class="h5">Backpack</h3>
+                <p class="text-sm text-muted">Deserunt et ad culpa culpa dolore.</p></a></div>
+            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/camera.3ee186fd.png" alt="" height="150">
+                <h3 class="h5">Camera</h3>
+                <p class="text-sm text-muted">Consectetur ex sunt duis minim quis dolor.</p></a></div>
+            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/glasses.80147afd.png" alt="" height="150">
+                <h3 class="h5">Sunglasses</h3>
+                <p class="text-sm text-muted">Deserunt et ad culpa culpa dolore.</p></a></div>
+            <div class="col-lg-3 col-md-6"><a class="text-reset" href="post.html"><img class="mb-4" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/headphone.cccb2497.png" alt="" height="150">
+                <h3 class="h5">Headphones</h3>
+                <p class="text-sm text-muted">Elit ad est labore irure qui.</p></a></div>
+          </div>
+        </div>
+      </section>
+      
       <gallery-section id="galeri" class="carousel slide carousel-fade p-5" data-bs-ride="carousel"></gallery-section>
       <hr />
 
