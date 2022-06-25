866e274fd4b77f25b63d12d9ddf57bda236c50a4
ayunovita123 <ayunovita7303@gmail.com>
2022-06-25 16:29:27 +0700

    menambah gambar

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -24,7 +24,49 @@ const Home = {
       <destination-recommendation class="portfolio pb-4" id="portfolio"></destination-recommendation>
       <hr />
 
-      <!-- Instagram section-->
+      <!--Instagram section-->
+    <section class="pb-5">
+      <div class="container pb-4">
+        <header class="text-center mb-5">
+          <h2>Backpack traveler</h2>
+          <p>A place for your Instagram pictures or gallery.</p>
+        </header>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-1.2aa0ba43.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-2.732bba03.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-3.0bbf93cd.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-4.95e66b76.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-5.3813454e.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-6.02c6ec6f.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-1.2aa0ba43.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+          <div class="col-lg-3 col-md-6 px-md-1 py-1"><a class="instagram-item d-block w-100 reset-anchor text-white" href="#!"><img class="img-fluid" src="https://d19m59y37dris4.cloudfront.net/travel/2-0/img/listing-tnumbnail-2.732bba03.jpg" alt="">
+              <div class="instagram-item-overlay p-5">
+                <h6>We travel not to escape life, but for life not to escape us.</h6>
+              </div></a></div>
+        </div>
+      </div>
+    </section>-->
       <section class="pb-5">
         <div class="container pb-4">
           <header class="text-center mb-5">
