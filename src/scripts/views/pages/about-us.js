61846380eafd6b5c720c23cf94261332068d4bfd
Bryan <mhdalana211201@gmail.com>
2022-06-29 01:15:17 +0700

    memperbaiki code dengan template content

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -1,131 +1,24 @@
+import dataTeam from "../../Data/team-data.json";
+import { createBioCardTeamTemplate } from "../templates/creator-templates";
+
 const AboutUs = {
   async render() {
     return `
-    <div class="container mt-5">
+    <div class="container bio-card">
       <div class="title">
         <h2>Tentang Kami</h2>
       </div>
-
-      <div class="container__detail d-flex align-items-center m-3 px-5 bg-info bg-opacity-50">
-        <img src="/media/bio/Alan.png" alt="Muhammad Alana Fauzan" class="container__image bg-secondary" />
-
-        <div class="container__inner m-5">
-          <h4 class="fw-bold text-center mb-3">Muhammad Alana Fauzan</h4>
-
-          <p class="description mx-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium libero eveniet illo at dolore, deserunt nam minus, et reiciendis soluta corrupti sint ratione accusantium cupiditate tempora consectetur?
-            Autem, tempora. Omnis!
-          </p>
-
-          <div class="social-media_link">
-            <a href="https://github.com/bryankazuro04/"><i class="fa-brands fa-github"></i>Github</a
-            >
-            <a
-              href="https://www.linkedin.com/in/muhammad-alana-fauzan-34b72b220/"
-              ><i class="fa-brands fa-linkedin"></i>LinkedIn</a
-            >
-            <a href="https://www.instagram.com/bryan.kazuro/"
-              ><i class="fa-brands fa-instagram"></i>Instagram</a
-            >
-            <a href="mailto:mhdalana211201@gmail.com"
-              ><i class="fa-solid fa-envelope"></i>E-Mail</a
-            >
-            <a href="https://wa.me/+6282119760841/?text=Hi, Nice to Meet You"
-              ><i class="fa-brands fa-whatsapp"></i>Whatsapp</a
-            >
-          </div>
-        </div>
-      </div>
-
-      <div
-        class="container__detail d-flex align-items-center m-3 px-5 bg-info bg-opacity-50"
-      >
-        <img src="" alt="Foto" class="container__image" />
-
-        <div class="container__inner m-5">
-          <h4 class="fw-bold text-center mb-3">(Nama)</h4>
-
-          <p class="description mx-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Laudantium libero eveniet illo at dolore, deserunt nam minus, et
-            reiciendis soluta corrupti sint ratione accusantium cupiditate
-            tempora consectetur? Autem, tempora. Omnis!
-          </p>
-
-          <div class="social-media_link">
-            <a href=""><i class="fa-brands fa-github"></i>Github</a>
-            <a href=""><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-            <a href=""><i class="fa-brands fa-instagram"></i>Instagram</a>
-            <a href=""><i class="fa-solid fa-envelope"></i>E-Mail</a>
-            <a href=""><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container__detail d-flex align-items-center m-3 px-5 bg-info bg-opacity-50">
-        <img src="" alt="Foto" class="container__image" />
-
-        <div class="container__inner m-5">
-          <h4 class="fw-bold text-center mb-3">(Nama)</h4>
-
-          <p class="description mx-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium libero eveniet illo at dolore, deserunt nam minus, et reiciendis soluta corrupti sint ratione accusantium cupiditate tempora consectetur?
-            Autem, tempora. Omnis!
-          </p>
-
-          <div class="social-media_link">
-            <a href=""><i class="fa-brands fa-github"></i>Github</a>
-            <a href=""><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-            <a href=""><i class="fa-brands fa-instagram"></i>Instagram</a>
-            <a href=""><i class="fa-solid fa-envelope"></i>E-Mail</a>
-            <a href=""><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container__detail d-flex align-items-center m-3 px-5 bg-info bg-opacity-50">
-        <img src="" alt="Foto" class="container__image" />
-
-        <div class="container__inner m-5">
-          <h4 class="fw-bold text-center mb-3">(Nama)</h4>
-
-          <p class="description mx-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium libero eveniet illo at dolore, deserunt nam minus, et reiciendis soluta corrupti sint ratione accusantium cupiditate tempora consectetur?
-            Autem, tempora. Omnis!
-          </p>
-
-          <div class="social-media_link">
-            <a href=""><i class="fa-brands fa-github"></i>Github</a>
-            <a href=""><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-            <a href=""><i class="fa-brands fa-instagram"></i>Instagram</a>
-            <a href=""><i class="fa-solid fa-envelope"></i>E-Mail</a>
-            <a href=""><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container__detail d-flex align-items-center m-3 px-5 bg-info bg-opacity-50">
-        <img src="" alt="Foto" class="container__image" />
-
-        <div class="container__inner m-5">
-          <h4 class="fw-bold text-center mb-3">(Nama)</h4>
-
-          <p class="description mx-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium libero eveniet illo at dolore, deserunt nam minus, et reiciendis soluta corrupti sint ratione accusantium cupiditate tempora consectetur?
-            Autem, tempora. Omnis!
-          </p>
-
-          <div class="social-media_link">
-            <a href=""><i class="fa-brands fa-github"></i>Github</a>
-            <a href=""><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-            <a href=""><i class="fa-brands fa-instagram"></i>Instagram</a>
-            <a href=""><i class="fa-solid fa-envelope"></i>E-Mail</a>
-            <a href=""><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
-          </div>
-        </div>
-      </div>
     </div>
     `;
+  },
+
+  async afterRender() {
+    const { teams } = dataTeam;
+    const bioCard = document.querySelector(".bio-card");
+
+    teams.forEach((team) => {
+      bioCard.innerHTML += createBioCardTeamTemplate(team);
+    });
   },
 };
 
