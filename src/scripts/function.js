cf324464bfb0ae33c8b2e2ecf38897ff6bf20f45
Bryan <mhdalana211201@gmail.com>
2022-06-29 12:14:32 +0700

    membuat dan menambahkan loader style dan direct import

--- src/scripts/function.js
+++ src/scripts/function.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "../styles/themes.css";
 import "../styles/main-style.css";
+import "../styles/loader.css";
 import "../styles/responsive.css";
 import "../styles/about-us.css";
 import "../styles/wisata.css";
