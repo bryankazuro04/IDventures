a52016e50a9a8ee91a4ceb80bb437820231caf13
Bryan <mhdalana211201@gmail.com>
2022-07-16 08:50:33 +0700

    menambahkan route halaman kontak

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,13 @@
 import Home from "../views/pages/home";
 import Explore from "../views/pages/explore";
 import AboutUs from "../views/pages/about-us";
+import Contact from "../views/pages/contact";
 
 const routes = {
   "/": Home,
   "/home": Home,
   "/explore": Explore,
+  "/contact": Contact,
   "/about-us": AboutUs,
 };
 
