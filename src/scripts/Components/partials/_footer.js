f4ec574252e06b1f4c8bca746779b02abf665561
Bryan <mhdalana211201@gmail.com>
2022-07-27 21:21:56 +0700

    mengubah tagname div menjadi section

--- src/scripts/Components/partials/_footer.js
+++ src/scripts/Components/partials/_footer.js
@@ -3,15 +3,15 @@ const footer = document.querySelector(".footer-32892");
 footer.innerHTML = `
 <div class="container">
   <div class="row">
-    <div class="col-sm col-md-6 mb-2 mb-md-0 pb-lg-4">
+    <section class="col-sm col-md-6 mb-2 mb-md-0 pb-lg-4">
       <h3 class="fw-bold">About IDVentures</h3>
 
       <p class="mb-2 text-start">
         IDVentures adalah media traveling Indonesia dengan spektrum yang lebih luas: tidak sekadar membahas obyek wisata dan destinasi umum, tetapi juga mengulas sisi-sisi lain yang mungkin bermanfaat bagi kalangan nonwisatawan.
       </p>
-    </div>
+    </section>
 
-    <div class="col-sm col-md-3 mb-md-0">
+    <section class="col-sm col-md-3 mb-md-0">
       <h3 class="fw-bold">Page</h3>
 
       <ul class="list-unstyled quick-info mb-2">
@@ -23,28 +23,28 @@ footer.innerHTML = `
           <a href="#/contact">Contact Us</a>
         </li>
       </ul>
-    </div>
+    </section>
 
-    <div class="col-sm col-md-3 mb-2 mb-md-0">
+    <section class="col-sm col-md-3 mb-2 mb-md-0">
       <h3 class="fw-bold">Contact</h3>
 
       <ul class="list-unstyled quick-info mb-2">
         <li>
-          <a href="#">
+          <a href="phoneto:+6223457812">
             <i class="fa-solid fa-phone icon"></i>+62 2345 7812
           </a>
         </li>
         
         <li>
-          <a href="#">
+          <a href="mailto:idventures@email.com">
             <i class="fa-regular fa-envelope icon"></i>idventures@email.com
           </a>
         </li>
       </ul>
-    </div>
+    </section>
 
     <hr />
-    <p class="text-start mb-3 fs-6">&copy; Developed by Team CPNG-80</p>
+    <p class="text-start text-white mb-3 fs-6">&copy; Developed by Team CPNG-80</p>
   </div>
 </div>
 `;
