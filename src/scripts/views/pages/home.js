ceae23744e388be2b18757ce656dda70f6adc1d2
Bryan <mhdalana211201@gmail.com>
2022-07-27 22:29:50 +0700

    menambahkan jumbotron, memindahkan carousel slide ke section 3, dan menambahkan function afterRender()

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,7 +1,38 @@
 const Home = {
   async render() {
     return `
-    <!-- Carousel -->
+    <section class="container py-5">
+      <div class="jumbotron__background row mb-4">
+        <span class="jumbotron__background-image">
+          <svg class="jumbotron__background-image--svg" id="visual" viewBox="0 0 900 450" width="900" height="450" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><g fill="#89e5f9"><circle r="33" cx="184" cy="146"></circle><circle r="21" cx="488" cy="409"></circle><circle r="20" cx="201" cy="52"></circle><circle r="20" cx="641" cy="111"></circle><circle r="29" cx="76" cy="367"></circle><circle r="28" cx="585" cy="175"></circle><circle r="26" cx="13" cy="48"></circle><circle r="25" cx="846" cy="256"></circle><circle r="29" cx="680" cy="417"></circle><circle r="21" cx="722" cy="333"></circle><circle r="26" cx="386" cy="231"></circle><circle r="23" cx="845" cy="69"></circle><circle r="21" cx="252" cy="328"></circle><circle r="23" cx="352" cy="324"></circle><circle r="21" cx="165" cy="226"></circle></g></svg>
+        </span>
+        <div class="jumbotron__description col-6 d-flex align-items-center">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam corporis aut quia itaque sequi similique nemo voluptatum architecto error quis atque impedit mollitia ullam fugiat, est minus? Dolorem qui numquam quas, dicta ducimus soluta alias perspiciatis dolores eaque voluptatem nam esse, voluptates est, officiis facere ad animi sed minima laboriosam.
+
+          </p>
+        </div>
+
+        <div class="jumbotron__picture col-6">
+          <img src="./media/jumbotron.png" alt="Jumbotron Picture" class="jumbotron__image w-100">
+          <span class="jumbotron__picture-background">
+            <svg class="jumbotron__picture-background--svg" id="visual" viewBox="0 0 900 600" width="900" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><g transform="translate(450.656850036715 309.9268138489584)"><path d="M180.6 -196.4C238.2 -166.9 291.9 -113.9 315.3 -46C338.7 21.9 331.7 104.7 284.8 144.2C237.9 183.7 151 180 80.6 193.5C10.1 207.1 -43.8 237.9 -103.7 237C-163.7 236 -229.5 203.3 -272.8 148C-316 92.7 -336.8 14.8 -327.4 -61.1C-318 -137 -278.6 -211 -219 -240.2C-159.4 -269.3 -79.7 -253.7 -9.1 -242.8C61.5 -232 123 -225.9 180.6 -196.4" fill="#d48be1"></path></g></svg>
+          </span>
+        </div>
+      </div>
+    </section>
+    `;
+  },
+
+  async afterRender() {
+    const container = document.querySelector("#mainContent section.container");
+
+    container.innerHTML += `
+    <best-recommendation></best-recommendation>
+    <hr />
+
+    <destination-recommendation></destination-recommendation>
+    
     <section id="carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
       <carousel-inner class="carousel-inner"></carousel-inner>
 
@@ -15,21 +46,10 @@ const Home = {
         <span class="visually-hidden">Next</span>
       </button>
     </section>
-    <!-- Carousel End -->
+    <hr />
 
-    <section class="container py-5">
-      <best-recommendation></best-recommendation>
-      <hr />
-
-      <destination-recommendation></destination-recommendation>
-      <hr />
-      
-      <gallery-section id="galeri" class="carousel slide carousel-fade p-5" data-bs-ride="carousel"></gallery-section>
-      <hr />
-
-      <nearest-destination class="row"></nearest-destination>
-      <hr />
-    </section>
+    <nearest-destination></nearest-destination>
+    <hr />
     `;
   },
 };
