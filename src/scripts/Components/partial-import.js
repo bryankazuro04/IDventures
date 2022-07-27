a836c8454eb7bc4335c87fbd47be3c4eb56ef6dc
Bryan <mhdalana211201@gmail.com>
2022-07-27 21:19:48 +0700

    menghapus direct import section_3.js

--- src/scripts/Components/partial-import.js
+++ src/scripts/Components/partial-import.js
@@ -3,6 +3,5 @@ import "./carousel";
 import "./main-container";
 import "./partials/section_1";
 import "./partials/section_2";
-import "./partials/section_3";
 import "./partials/section_4";
 import "./partials/_footer";
