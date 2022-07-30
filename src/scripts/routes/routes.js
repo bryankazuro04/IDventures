ee0784c77e3ae21e761156a7a42142d7acd7811f
Bryan <mhdalana211201@gmail.com>
2022-07-30 23:12:37 +0700

    menambahkan route halaman destinasi

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,11 +2,12 @@ import Home from "../views/pages/home";
 import Explore from "../views/pages/explore";
 import AboutUs from "../views/pages/about-us";
 import Contact from "../views/pages/contact";
+import Destination from "../views/pages/destination";
 
 const routes = {
   "/": Home,
-  "/home": Home,
   "/explore": Explore,
+  "/explore/:id": Destination,
   "/contact": Contact,
   "/about-us": AboutUs,
 };
