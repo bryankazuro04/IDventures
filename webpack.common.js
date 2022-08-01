2b8f43fb7d874003003caaa66e67ae7a68ada307
Bryan <mhdalana211201@gmail.com>
2022-08-01 23:51:42 +0700

    install plugin workbox webpack, webpack PWA Manifest, workbox-window, and add configure webpackPWAManifest and workbox GenerateSW

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/scripts/function.js"),
@@ -38,6 +40,41 @@ module.exports = {
           to: path.resolve(__dirname, "./dist/media/"),
         },
       ],
+    }),
+    new WebpackPwaManifest({
+      name: "IDventures",
+      short_name: "IDventures",
+      description:
+        "Website untuk mencari dan menemukan tempat wisata yang sangat unik dan jarang diketahui banyak orang",
+      background_color: "#eeeeee",
+      theme_color: "#8edeeb",
+      crossorigin: "use-credentials",
+      icons: [
+        {
+          src: path.resolve("src/media/icons/favicon-16.png"),
+          size: "16x16",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("src/media/icons/favicon-32.png"),
+          size: "32x32",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("src/media/icons/favicon-96.png"),
+          size: "96x96",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("src/media/icons/favicon-192.png"),
+          size: "192x192",
+          type: "image/png",
+        },
+      ],
+    }),
+    new GenerateSW({
+      swDest: "./sw.bundle.js",
+      maximumFileSizeToCacheInBytes: 3000000,
     }),
   ],
   optimization: {
