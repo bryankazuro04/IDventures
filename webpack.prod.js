2bc19c60a025c8f3634b0538bb10e02cce44d663
Bryan <mhdalana211201@gmail.com>
2022-08-01 15:05:39 +0700

    menginstall plugin lazysizes & imagemin webp dan membuat config untuk generate file image menjadi webp

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,36 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: "/node_modules/",
-      use: [{
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
-        },
-      }, ],
-    }, ],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
   },
+  plugins: [
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 70,
+          },
+        },
+      ],
+      overrideExtension: true,
+    }),
+  ],
 });
